523db4fd7808e132d9a47cb7e840834d9d9bacf3
Faisal <aryuical@gmail.com>
2019-10-31 19:19:30 +0700

    Login and Register Ajax Request

--- script/main.js
+++ script/main.js
@@ -1,11 +1,56 @@
-// function login() {
-//     $.ajax({
-//         method: "post",
-//         data: {
+function login() {
+    let email = $("#username").val();
+    let password = $("#password").val();
+    $.ajax({
+        method: "post",
+        url: "http://localhost:3000/user/login",
+        data: {
+            email,
+            password
+        }
+    }).done((result) => {
+        localStorage.setItem("token", result.token);
+        Swal.fire({
+            type: 'success',
+            title: 'Success',
+            text: 'Login Successfully',
+        })
+    }).fail((err) => {
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+        })
+    })
+}
 
-//         }
-//     })
-// }
+function register() {
+    let name = $("#name").val();
+    let email = $("#username").val();
+    let password = $("#password").val();
+
+    $.ajax({
+        method: "post",
+        url: "http://localhost:3000/user/register",
+        data: {
+            name,
+            email,
+            password
+        }
+    }).done((result) => {
+        Swal.fire({
+            type: 'success',
+            title: 'Success',
+            text: 'Register Successfully',
+        })
+    }).fail((err) => {
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+        })
+    })
+}
 
 function onSignIn(googleUser) {
     let id_token = googleUser.getAuthResponse().id_token;
@@ -18,6 +63,17 @@ function onSignIn(googleUser) {
         }
     }).done((result) => {
         localStorage.setItem("token", result.token);
+        Swal.fire({
+            type: 'success',
+            title: 'Success',
+            text: 'Login Successfully',
+        })
+    }).fail((err) => {
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+        })
     })
 
 }
